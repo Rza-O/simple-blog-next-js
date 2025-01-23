8fa2e53e01a84927c290ea99263f9bd4a41c7109
appoclayptor <shahreza1414@gmail.com>
2025-01-23 13:49:47 +0600

    fix: ensure proper redirection for unauthenticated users in profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,7 +6,7 @@ const profile = async () => {
    const { isAuthenticated } = getKindeServerSession();
    const authenticated = await isAuthenticated();
    if (!authenticated) {
-      redirect("/api/auth/login");
+      return redirect("/api/auth/login");
    }
    return (
       <div>
