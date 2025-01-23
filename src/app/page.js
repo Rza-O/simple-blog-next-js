df8e12ace370f10364020794464559e00e95f210
appoclayptor <shahreza1414@gmail.com>
2025-01-23 13:06:01 +0600

    add axios dependency and fetch posts in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
 import React from 'react';
 
-const Home = () => {
+const Home = async () => {
+   const { data } = await axios('https://jsonplaceholder.typicode.com/posts');
+   console.log(data)
    return (
       <div>
          <h2 className='text-5xl'>This is home page!</h2>
